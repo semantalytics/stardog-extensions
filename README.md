44823e7e55cc9265b67d3270310287f2a636131f
Zachary Whitley <zcw100@localhost.localdomain>
2020-04-26 21:19:27 -0400

    Initial commit from funciton-util-sayNumber

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# stardog-functions
-Single repository for all Stardog function extensions
+[![Build Status](https://travis-ci.org/semantalytics/stardog-kibbles.svg?branch=master)](https://travis-ci.org/semantalytics/stardog-kibbles)
+
+# Stardog Kibbles
+
+A collection of [Stardog](http://stardog.com) plugins
+
---
+++ src/test/java/com/semantalytics/stardog/function/utils/TestSayNumber.java
@@ -0,0 +1,37 @@
+package com.semantalytics.stardog.function.utils;
+
+import com.semantalytics.stardog.function.FunctionName;
+import com.stardog.stark.Literal;
+import com.stardog.stark.Value;
+import com.stardog.stark.query.SelectQueryResult;
+import com.semantalytics.stardog.AbstractStardogTest;
+import org.junit.*;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestSayNumber extends AbstractStardogTest {
+
+    @Test
+    public void testFromSpokenTime() {
+
+            final String aQuery = "prefix util: <" + FunctionName.Utils.sayNumber.NAMESPACE + ">" +
+                    "select ?result where { bind(util:sayNumber(532) as ?result) }";
+
+            try (final SelectQueryResult aResult = connection.select(aQuery).execute()) {
+
+                assertThat(aResult.hasNext()).isTrue();
+
+                final Optional<Value> aPossibleValue = aResult.next().value("result");
+
+                assertThat(aPossibleValue).isPresent();
+
+                final Value aValue = aPossibleValue.get();
+
+                assertThat(aValue).isInstanceOf(Literal.class);
+                assertThat(((Literal)aValue).label()).isEqualTo("five hundred and thirty-two");
+                assertThat(aResult.hasNext()).isFalse();
+            }
+    }
+}
